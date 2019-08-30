cc977251e1f5100c6ae629907a84511b1b19096c
Steven Bowman <stevenbowmanjr@gmail.com>
2019-08-30 00:24:22 -0700

    created loop that records how many letters are in the word variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,13 @@
+//word list array============
+
 var words = ["zeppelin", "maiden",]
 
+//word selector======================
 var word = words[Math.floor(Math.random()* words.length)];
 console.log(word)
+
+//Blank Letter Generator
+var answerArray =[];
+for (var i = 0; i < word.length; i++){
+    answerArray[i] = "_";
+}
