9ff2da4662b4dde5dd0bfa7f8ea0c537bcb260c3
Steven Bowman <stevenbowmanjr@gmail.com>
2019-08-31 14:53:09 -0700

    the blank letters of answerArray written into the paragraph id word.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,36 @@
 var words = ["zeppelin", "maiden",]
 
 //word selector======================
-var word = words[Math.floor(Math.random()* words.length)];
+var word = words[Math.floor(Math.random() * words.length)];
 console.log(word)
 
-//Blank Letter Generator
-var answerArray =[];
-for (var i = 0; i < word.length; i++){
+//Blank Letter Generator===============
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_";
 }
+console.log(answerArray)
+
+// Remaining letters==================
+var remainingLetters = word.length;
+console.log(remainingLetters)
+//======================generating alphabet array============
+function genCharArray(charA, charZ) {
+    var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
+    for (; i <= j; ++i) {
+        a.push(String.fromCharCode(i));
+    }
+    return a;
+}
+var alphabet = genCharArray('a', 'z'); // ["a", ..., "z"]
+console.log(alphabet);
+
+
+
+document.getElementById("word").innerHTML = answerArray.join(" ");
+        // document.getElementById('hello').innerHTML = 'hi';
+
+
+
+//=================================GAME=============================
+
