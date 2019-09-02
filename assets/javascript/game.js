e85e90b8d49c620b875d8f9bcb3b6531d523133e
Steven Bowman <stevenbowmanjr@gmail.com>
2019-09-02 13:21:11 -0700

    the main game function which was keyevent and if conditional making sure guess was a-z char, inside that conditional a new conditional if guess in already in the guessed array an alert tells you to pick another letter, had to create another variable guess2 to make it not say already used the first timeyou guessed a letter. the else portion pushes the new guess into the guessed array and then displays the array. I added variable for each paragraph id

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,23 @@
 //word list array============
-
-var words = ["zeppelin", "maiden",]
+let displayWins = document.getElementById("wins");
+let displayWord = document.getElementById("word");
+let displayGuesses = document.getElementById("guesses")
+var words = ["zeppelin", "maiden",];
 
 //word selector======================
 var word = words[Math.floor(Math.random() * words.length)];
-console.log(word)
+console.log(word);
 
 //Blank Letter Generator===============
 var answerArray = [];
 for (let i = 0; i < word.length; i++) {
     answerArray[i] = "_";
 }
-console.log(answerArray)
+console.log(answerArray);
 
 // Remaining letters==================
 var remainingLetters = word.length;
-console.log(remainingLetters)
+console.log(remainingLetters);
 //======================generating alphabet array============
 
 function genCharArray(charA, charZ) {
@@ -30,14 +32,26 @@ console.log(alphabet);
 
 
 //=================================GAME=============================
+let guessed = [];
+
 
 document.onkeyup = function (event) {
     let guess = event.key.toLowerCase();
-    
-    if (alphabet.indexOf(guess) >= 0) {
-        alert("it worked")
-    }
+    let guess2 = guess;
 
-    
+    // if the guess is in the alphabet
+    if (alphabet.indexOf(guess) !== -1) {
+        // alert("it worked")
+
+        if (guessed.indexOf(guess2) !== -1) {
+            alert(guessed + " already used.")
+
+        }
+        else {
+            guessed.push(guess2);
+            console.log(guessed)
+            
+        }
+    }
 }
 
